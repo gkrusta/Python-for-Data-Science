3612d6ad6f06e0dde0f639f597775b02fbcb3e59
gkrusta <krustagabriela@gmail.com>
2024-12-12 15:29:03 +0100

    tester add

--- starting/ex08/Loading.py
+++ starting/ex08/Loading.py
@@ -1,9 +1,13 @@
-def ft_tqdm(lst: range) -> None:
-
+#def ft_tqdm(lst: range) -> None:
+from time import sleep
+from tqdm import tqdm
 def main():
-    
+    for elem in tqdm(range(200)):
+        sleep(0.5)
+        yield elem
  
 
 
 if __name__ == "__main__":
-    main()
+    for value in main():
+        pass
