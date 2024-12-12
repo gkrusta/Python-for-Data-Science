a3b10f861f918289c4563e2794bac64ba9499fbe
gkrusta <krustagabriela@gmail.com>
2024-12-12 15:55:59 +0100

    load

--- starting/ex08/Loading.py
+++ starting/ex08/Loading.py
@@ -1,13 +1,20 @@
-#def ft_tqdm(lst: range) -> None:
-from time import sleep
-from tqdm import tqdm
-def main():
-    for elem in tqdm(range(200)):
+def ft_tqdm(lst: range) -> None:
+    progress = 
+    field_legnth = 
+    painted = 
+    precentage = 
+    for i, elem in enumerate(lst, start=1):
+        
+
+    
+
+def main(range):
+    for elem in ft_tqdm(range(200)):
         sleep(0.5)
         yield elem
  
 
 
 if __name__ == "__main__":
-    for value in main():
-        pass
+    range = 100
+    main(range)
